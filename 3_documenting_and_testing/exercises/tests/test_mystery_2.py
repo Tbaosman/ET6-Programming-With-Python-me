b69c87abf14f194703a098b0b307657bc85af750
Tbaosman <99553124+Tbaosman@users.noreply.github.com>
2024-12-17 20:43:38 +0400

    added tests to mystery2

--- 3_documenting_and_testing/exercises/tests/test_mystery_2.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_2.py
@@ -12,3 +12,15 @@ class TestMystery2(unittest.TestCase):
     def test_minimal_string_input(self):
         """"""
         self.assertEqual(mystery_2(''), None)
+        
+    def test_string(self):
+        """"""
+        self.assertEqual(mystery_2("tbaosman"), 8)
+    
+    '''def test_integers(self):
+        """"""
+        self.assertEqual(mystery_2(12345), 5)'''
+        
+    def test_combination(self):
+        """"""
+        None
