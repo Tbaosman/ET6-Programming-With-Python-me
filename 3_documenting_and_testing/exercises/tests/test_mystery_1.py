008f32789bda7745c738a110a68d45b6fbbceff7
Tbaosman <99553124+Tbaosman@users.noreply.github.com>
2024-12-17 20:08:12 +0400

    Test1

--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -8,3 +8,12 @@ class TestMystery1(unittest.TestCase):
     def test_minimal_input(self):
         """"""
         self.assertEqual(mystery_1(0, 0), 0)
+    def test_positive(self):
+        """"""
+        self.assertEqual(mystery_1(2,5), 7)
+    def test_combination(self):
+        """"""
+        self.assertEqual(mystery_1(-2,2), 0)
+    def test_negative(self):
+        """"""
+        self.assertEqual(mystery_1(-2,-4), -6)
